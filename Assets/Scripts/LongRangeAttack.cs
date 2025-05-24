65aa7b85cf2e720825a74651712d71eef53e5068
Emily Krohn <emilykrohn@csu.fullerton.edu>
2025-05-24 08:12:33 -0700

    Start Long Range Attack Script
    Every 0.5 seconds, the attack function is run

---
+++ Assets/Scripts/LongRangeAttack.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class LongRangeAttack : MonoBehaviour, IAttack
+{
+    [SerializeField] private Entity _player;
+    [SerializeField] private int _attack_amount = 1;
+    public Entity CurrentEntity { get; set; }
+    public float timer = 0f;
+    public float delay = 0.5f;
+    public int AttackAmount
+    {
+        get
+        {
+            return _attack_amount;
+        }
+        set
+        {
+            // Makes sure attack amount is above zero or prints out that it needs to be above zero
+            if (value > 0)
+            {
+                _attack_amount = value;
+            }
+            else
+            {
+                Debug.Log("Attack amount needs to be greater than 0");
+            }
+        }
+    }
+
+    public void Attack()
+    {
+        Debug.Log("Attack");
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > delay)
+        {
+            timer = 0f;
+            Attack();
+        }
+    }
+}
